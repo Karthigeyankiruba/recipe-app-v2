73b82cdf9bcbef1d83c324ae2c733a82258944d6
Karthigeyankiruba <karthigeyan0901@gmail.com>
2024-02-26 17:06:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -22,29 +22,10 @@ function App() {
 
   // console.log("data: ", data);
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Recipe App V2 Started
-        </a>
-      </header>
-    </div>
-=======
     <>
       {/* <SignUpForm /> */}
       <ResponsiveAppBar />
     </>
->>>>>>> main
   );
 }
 
