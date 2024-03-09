6de54a3fc1b64d7d267a56f61e8379eddc1da152
Karthigeyankiruba <karthigeyan0901@gmail.com>
2024-03-09 19:53:18 +0530

    changes

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>404Found</div>;
+  return (
+    <img
+      src="https://cdn.dribbble.com/userupload/8333260/file/original-6282cbbe1faf7fbd86bd96a2d9d735b2.jpg?resize=3629x1800"
+      width={"100%"}
+    />
+  );
 };
 
 export default NotFound;
